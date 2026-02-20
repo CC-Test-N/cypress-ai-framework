66198c55d38f98a5025a2248c6aabbc0099f8072
NataliiaStetsyk <34649004+NataliiaStetsyk@users.noreply.github.com>
2026-02-20 15:54:17 -0600

    Update comment for image snapshot command registration

--- support/index.ts
+++ support/index.ts
@@ -2,6 +2,6 @@ require('./commands.ts')
 require('./ai.ts')
 require('cypress-mochawesome-reporter/register')
 
-// Register image snapshot command
+// Register image snapshot command test
 const { addMatchImageSnapshotCommand } = require('cypress-image-snapshot/command')
 addMatchImageSnapshotCommand()
