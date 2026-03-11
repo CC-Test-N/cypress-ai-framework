7f36651f328afa264518a1702f9afae770989318
NataliiaStetsyk <34649004+NataliiaStetsyk@users.noreply.github.com>
2026-03-11 16:19:10 -0500

    Correct comment typo in index.ts
    Fix typo in comment for image snapshot command registration.

--- support/index.ts
+++ support/index.ts
@@ -2,6 +2,6 @@ require('./commands.ts')
 require('./ai.ts')
 require('cypress-mochawesome-reporter/register')
 
-// Register image snapshot command
+// Register image snapshot command teset
 const { addMatchImageSnapshotCommand } = require('cypress-image-snapshot/command')
 addMatchImageSnapshotCommand()
